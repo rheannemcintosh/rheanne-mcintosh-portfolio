43da7758e5bb0ab01734dfdb7d8aeac0642adf01
rheannemcintosh <rheannemcintosh@gmail.com>
2024-09-08 19:59:12 +0100

    Create a CV Heading
    [RM-7]

---
+++ resources/js/Components/CVHeading.vue
@@ -0,0 +1,24 @@
+<script setup>
+    const props = defineProps({
+        title: String,
+        icon: String,
+    });
+
+</script>
+
+<template>
+    <dv class="flex items-center text-green-700 ">
+            <span class="material-symbols-rounded text-4xl">
+                {{ icon }}
+            </span>
+        <h1 class="pl-2 font-bold text-4xl">
+            {{ title }}
+        </h1>
+    </dv>
+
+
+</template>
+
+<style scoped>
+
+</style>
