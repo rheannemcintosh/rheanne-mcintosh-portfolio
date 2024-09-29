3f317f4096333c1617eb6376ab1f2f2a4838ba1d
rheannemcintosh <rheannemcintosh@gmail.com>
2024-09-29 23:49:02 +0100

    Add the Navigation to the CV page
    [RM-27]

--- resources/js/Components/Unique/NavigationLinks.vue
+++ resources/js/Components/Unique/NavigationLinks.vue
@@ -4,7 +4,7 @@
 
 <template>
     <NavigationLink :href="route('home')" :active="route().current('home')">Home</NavigationLink>
-    <NavigationLink :href="route('cv')" :active="route().current('cv')">Timeline</NavigationLink>
+    <NavigationLink :href="route('cv')" :active="route().current('cv')">CV</NavigationLink>
 <!--    <NavigationLink :href="route('skills')" :active="route().current('skills')">Skills</NavigationLink>-->
 <!--    <NavigationLink :href="route('timeline')" :active="route().current('timeline')">Timeline</NavigationLink>-->
 <!--    <NavigationLink :href="route('contact')" :active="route().current('contact')">Contact</NavigationLink>-->
--- resources/js/Pages/CV/Index.vue
+++ resources/js/Pages/CV/Index.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue';
 import Pill from "@/Components/Unique/Pill.vue";
 import Education from "@/Pages/Education/Index.vue";
 import Employers from "@/Pages/Employers/Index.vue";
+import Navigation from "@/Components/Unique/Navigation.vue";
 
 const props = defineProps<{
     degrees: Degree[];
@@ -19,15 +20,18 @@ const handlePillClick = (pillName: string) => {
 </script>
 
 <template>
-    <div class="my-8">
-        <pill name="All" pillColour="green-pill" @pill-click="handlePillClick"/>
-        <pill name="Employers" pillColour="green-pill" @pill-click="handlePillClick"/>
-        <pill name="Education" pillColour="green-pill" @pill-click="handlePillClick"/>
-    </div>
+    <Navigation />
+    <div class="m-4 max-w-3xl mx-auto">
+        <div class="my-8">
+            <pill name="All" pillColour="green-pill" @pill-click="handlePillClick"/>
+            <pill name="Employers" pillColour="green-pill" @pill-click="handlePillClick"/>
+            <pill name="Education" pillColour="green-pill" @pill-click="handlePillClick"/>
+        </div>
 
-    <div class="content">
-        <Employers v-if="selectedPill === 'All' || selectedPill === 'Employers'" :employers="props.employers" />
-        <Education v-if="selectedPill === 'All' || selectedPill === 'Education'" :degrees="props.degrees" />
+        <div class="content">
+            <Employers v-if="selectedPill === 'All' || selectedPill === 'Employers'" :employers="props.employers" />
+            <Education v-if="selectedPill === 'All' || selectedPill === 'Education'" :degrees="props.degrees" />
+        </div>
     </div>
 </template>
 
