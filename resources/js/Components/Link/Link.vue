c4bc8b7ed6db7585f751b48a893597ecd35bd80e
rheannemcintosh <rheannemcintosh@gmail.com>
2024-10-15 23:46:32 +0100

    Create a Link Vue component
    [RM-47]

---
+++ resources/js/Components/Link/Link.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+    // Import the relevant types
+    import { Link } from "@/types/Link/Link";
+
+    // Define the props
+    const props = defineProps<Link>();
+
+</script>
+
+<template>
+
+    <a :href="props.url">
+        {{ props.text}}
+    </a>
+
+</template>
+
+<style scoped>
+    a {
+        @apply text-rmAccent underline font-bold;
+    }
+</style>
