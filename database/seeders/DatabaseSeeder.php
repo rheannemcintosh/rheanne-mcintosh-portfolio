2657c29cd001144b298ecd893e0ade0d3bae6fd3
rheannemcintosh <rheannemcintosh@gmail.com>
2024-10-18 01:07:41 +0100

    Update the database seeder to conditionally call custom seeders
    [RM-66]

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,10 +4,28 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Seeders\custom\AboutSeeder;
+use Database\Seeders\custom\DegreeSeeder;
+use Database\Seeders\custom\EducationSeeder;
+use Database\Seeders\custom\EmployerSeeder;
+use Database\Seeders\custom\ResponsibilitySeeder;
+use Database\Seeders\custom\RoleSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * @var string[] $customSeeders
+     */
+    private array $customSeeders = [
+        'database/seeders/custom/AboutSeeder.php'          => AboutSeeder::class,
+        'database/seeders/custom/DegreeSeeder.php'         => DegreeSeeder::class,
+        'database/seeders/custom/EducationSeeder.php'      => EducationSeeder::class,
+        'database/seeders/custom/EmployerSeeder.php'       => EmployerSeeder::class,
+        'database/seeders/custom/RoleSeeder.php'           => RoleSeeder::class,
+        'database/seeders/custom/ResponsibilitySeeder.php' => ResponsibilitySeeder::class,
+    ];
+
     /**
      * Seed the application's database.
      */
@@ -19,5 +37,11 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        foreach ($this->customSeeders as $file => $seederClass) {
+            if (file_exists(base_path($file))) {
+                $this->call($seederClass);
+            }
+        }
     }
 }
