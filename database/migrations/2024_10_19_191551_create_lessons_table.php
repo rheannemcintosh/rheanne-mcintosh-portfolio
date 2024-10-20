547b8597a87f8583afd14eb11bb407a381dc5758
rheannemcintosh <rheannemcintosh@gmail.com>
2024-10-20 20:00:51 +0100

    Create the lessons table
    [RM-69]

---
+++ database/migrations/2024_10_19_191551_create_lessons_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('course_id')->nullable()->constrained()->onDelete('cascade');
+            $table->timestamp('completed_at')->nullable();
+            $table->integer('number')->nullable();
+            $table->string('section')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
