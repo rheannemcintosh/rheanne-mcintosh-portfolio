a1663bb03f8e584d792c2c9813701cc847126d2a
rheannemcintosh <rheannemcintosh@gmail.com>
2024-12-29 23:58:18 +0000

    Create sections on the home page and adjust the size of the navigation
    [RM-76]

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -9,4 +9,10 @@
 </template>
 
 <style scoped lang="postcss">
+    .section {
+        height: calc(100svh - 9rem); /* Adjust for nav height */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
