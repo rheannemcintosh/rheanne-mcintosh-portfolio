ffb3643e380ecafb7c9ce199005d8b45e7cdf676
rheannemcintosh <rheannemcintosh@gmail.com>
2024-12-30 03:41:53 +0000

    Add secondary navigation links for the home page
    [RM-83]

--- resources/js/Components/Unique/Navigation.vue
+++ resources/js/Components/Unique/Navigation.vue
@@ -7,6 +7,14 @@
     const toggleMobileMenu = () => {
         isMobileMenuVisible.value = !isMobileMenuVisible.value;
     };
+
+    // Smooth scroll function
+    const scrollToSection = (id: string) => {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({ block: "end", behavior: "smooth" });
+        }
+    };
 </script>
 
 <template>
@@ -27,6 +35,14 @@
                         <NavigationLinks />
                     </div>
 
+                    <!-- Secondary Navigation for Home Page -->
+                    <div id="home-secondary-navigation" class="hidden md:flex md:space-x-6 md:text-rmLight md:text-bold justify-center items-center text-xs border-t-2 border-rmAccent">
+                        <a href="#hello" @click.prevent="scrollToSection('hello')" class="cursor-pointer px-4 pt-4">Hello</a>
+                        <a href="#about" @click.prevent="scrollToSection('about')" class="cursor-pointer px-4 pt-4">About</a>
+                        <a href="#experience" @click.prevent="scrollToSection('experience')" class="cursor-pointer px-4 pt-4">Experience</a>
+                        <a href="#education" @click.prevent="scrollToSection('education')" class="cursor-pointer px-4 pt-4">Education</a>
+                    </div>
+
                 <!-- Burger Icon for Mobile -->
                 <div id="burger-icon" class="md:hidden flex">
                     <span class="material-symbols-rounded text-4xl text-rmLight cursor-pointer" @click="toggleMobileMenu">
@@ -44,6 +60,11 @@
             <div class="bg-rmDark text-rmLight p-4">
                 <div class="space-y-4">
                     <NavigationLinks />
+                    <hr class="border-rmLight/50 my-4">
+                    <a href="#hello" @click.prevent="scrollToSection('about')" class="block cursor-pointer">About</a>
+                    <a href="#about" @click.prevent="scrollToSection('about')" class="block cursor-pointer">About</a>
+                    <a href="#experience" @click.prevent="scrollToSection('experience')" class="block cursor-pointer">Experience</a>
+                    <a href="#education" @click.prevent="scrollToSection('education')" class="block cursor-pointer">Education</a>
                 </div>
             </div>
         </div>
